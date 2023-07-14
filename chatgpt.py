d43b66b4dc4c7541c16e9d16cc2455fa6829bab1
l3limp <kakarvardaan@gmail.com>
2023-07-14 17:56:46 +0530

    changed llm

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from langchain.chat_models import ChatOpenAI
+
 import constants
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
@@ -12,4 +14,6 @@ print(query)
 
 loader = TextLoader('data.txt')
 index = VectorstoreIndexCreator().from_loaders([loader])
-print(index.query(query))
+
+# print(index.query(query))
+print(index.query(query, llm=ChatOpenAI()))
